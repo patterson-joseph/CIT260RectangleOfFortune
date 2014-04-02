0ba28dc5faaebaa899354856116e0ef472eba976
Crossley Family <Crossley Family@FamilyRoom>
2014-04-02 00:25:36 -0700

    Initial Jframe addition for week 12 paired programming assignment.

--- src/BYUI/CIT260/RectangleOfFortune/menu/controls/RectangleOfFortune.java
+++ src/BYUI/CIT260/RectangleOfFortune/menu/controls/RectangleOfFortune.java
@@ -1,7 +1,8 @@
 package BYUI.CIT260.RectangleOfFortune.menu.controls;
 
+import BYUI.CIT260.RectangleOfFortune.frames.MainFrame;
 import BYUI.CIT260.RectangleOfFortune.views.Messages;
-import BYUI.CIT260.RectangleOfFortune.views.MainMenuView;
+//import BYUI.CIT260.RectangleOfFortune.views.MainMenuView;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -18,22 +19,47 @@ public class RectangleOfFortune {
             + "button to start your turn.  Player 1 goes first.\n\n";
     
     private final static Scanner inFile = new Scanner(System.in);
-    
+    public MainFrame mainFrame = null;
     public static void main(String[] args) {
         
-        //display our splash page
-        displayIntro();
-        //call the main menu up
-        MainMenuView mainMenu = new MainMenuView();
+//        //display our splash page
+//        displayIntro();
+//        //call the main menu up
+//        MainMenuView mainMenu = new MainMenuView();
+        
+        
+        
+//        try {
+//            mainMenu.executeCommands();
+//        } catch (Throwable e) {
+//            Messages.displayError("Unexpected Error: " + e.getMessage());
+//            Messages.displayError(Arrays.toString(e.getStackTrace()));
+//        } finally {
+//            RectangleOfFortune.inFile.close();
+//        }
+        
+//        RectangleOfFortune rectangleOfFortune = null;
+        final RectangleOfFortune rectangleOfFortune=new RectangleOfFortune();
         
         try {
-            mainMenu.executeCommands();
-        } catch (Throwable e) {
-            Messages.displayError("Unexpected Error: " + e.getMessage());
-            Messages.displayError(Arrays.toString(e.getStackTrace()));
+            /* Create and display jframe form */
+            java.awt.EventQueue.invokeLater(new Runnable() {
+               @Override
+               public void run() {
+                   rectangleOfFortune.mainFrame = new MainFrame();
+                   
+                   rectangleOfFortune.mainFrame.setVisible(true);
+               }
+            });
+        } catch (Throwable ex){
+            Messages.displayError("Unexpected Error: " + ex.getMessage());
+            Messages.displayError(Arrays.toString(ex.getStackTrace()));
         } finally {
-            RectangleOfFortune.inFile.close();
+            if (rectangleOfFortune.mainFrame != null) {
+                rectangleOfFortune.mainFrame.dispose();
+            }
         }
+        
     }
     
     public static void displayIntro() {
